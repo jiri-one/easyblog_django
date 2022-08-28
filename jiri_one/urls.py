9d9f469c73c108a8725891965a7fac1676218e64
jiri.one <nemec@jiri.one>
2022-08-28 21:55:17 +0200

    working pagination with page

--- jiri_one/urls.py
+++ jiri_one/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "jiri_one"
 urlpatterns = [
     path('', views.PostListView.as_view()),
+    path('page/<int:page>', views.PostListView.as_view()),
+    path('strana/<int:strana>', views.PostListView.as_view()),
     path('<slug:url_cze>/', views.PostDetailView.as_view()),
 ]
