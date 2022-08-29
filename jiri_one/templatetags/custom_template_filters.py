4a3d159f43d237f7b1c38023f62988722eb39a28
jiri.one <nemec@jiri.one>
2022-08-29 22:19:18 +0200

    finished pagination

--- jiri_one/templatetags/custom_template_filters.py
+++ jiri_one/templatetags/custom_template_filters.py
@@ -11,3 +11,8 @@ def tags_html(tags):
              f"""<a href="/tag/{tag["url_cze"]}">{tag["name_cze"]}</a>"""
         )
     return ", ".join(tags_links)
+
+@register.filter
+def rm_pg_nr(path: str):
+    """From url path remove the last part, which is page number"""
+    return "/".join(path.split("/")[:-1])
