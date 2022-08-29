681e08d4e4985ea044c4d84f7988050e2440365e
jiri.one <nemec@jiri.one>
2022-08-29 15:40:22 +0200

    finished tags and to add comments is possible

--- jiri_one/urls.py
+++ jiri_one/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('tag/<str:tag>', views.PostListView.as_view()),
     path('tag/<str:tag>/strana/<int:strana>', views.PostListView.as_view()),
     path('tag/<str:tag>/page/<int:page>', views.PostListView.as_view()),
+    path('search/<str:search>', views.PostListView.as_view()),
+    path('hledej/<str:hledej>', views.PostListView.as_view()),
+    path('search/<str:search>/strana/<int:strana>', views.PostListView.as_view()),
+    path('hledej/<str:hledej>/page/<int:page>', views.PostListView.as_view()),
     path('<slug:url_cze>/', views.PostDetailView.as_view()),
 ]
--- jiri_one/views.py
+++ jiri_one/views.py
@@ -1,27 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from jiri_one.models import Post, Tag, Comment
+from jiri_one.models import Post, Comment
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.db.models import Q
+from django.shortcuts import redirect
 
 class PostDetailView(DetailView):
     model = Post
     slug_field = 'url_cze'
     slug_url_kwarg = 'url_cze'
     template_name = 'post.html'
+    
+    def post(self, request, *args, **kwargs):
+        header = request.POST.get('comment_header')
+        nick = request.POST.get('comment_nick')
+        content = request.POST.get('comment_content')
+        Comment.objects.create(post=self.get_object(), title=header, nick=nick,content=content)
+        return redirect(request.path)
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'index.html'
     paginate_by = 10
-    
-    
+        
     def get(self, request, *args, **kwargs):
         if 'strana' in kwargs:
             self.page_kwarg = 'strana'
         if 'tag' in kwargs:
-            tag = Tag.objects.get(url_cze=kwargs["tag"])
-            self.queryset = self.model.objects.filter(tags__exact=tag).all()
+            self.queryset = self.model.objects.filter(tags__url_cze=kwargs["tag"]).all()
+        if 'search' in kwargs or 'hledej' in kwargs:
+            if not (searched_word := kwargs.get("search")):
+                searched_word = kwargs.get("hledej")
+            self.queryset = self.model.objects.filter(
+                Q(content_cze__icontains=searched_word) 
+                | Q(title_cze__icontains=searched_word))
         return super().get(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        searched_word = request.POST.get('search') 
+        return redirect(f'hledej/{searched_word}')
     
