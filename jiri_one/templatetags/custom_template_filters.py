cd947a7daeab4ac5ce45ed580df15687ab3f0072
jiri.one <nemec@jiri.one>
2022-08-30 08:15:21 +0200

    fixed paginations

--- jiri_one/templatetags/custom_template_filters.py
+++ jiri_one/templatetags/custom_template_filters.py
@@ -15,4 +15,5 @@ def tags_html(tags):
 @register.filter
 def rm_pg_nr(path: str):
     """From url path remove the last part, which is page number"""
-    return "/".join(path.split("/")[:-1])
+    return "/".join(list(filter(None, path.split("/")))[:-1])
+    # return "/".join([x for x in path.split("/") if x != ""][:-1])   
--- jiri_one/urls.py
+++ jiri_one/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name = "jiri_one"
 urlpatterns = [
     path('', views.PostListView.as_view()),
-    path('page/<int:page>', views.PostListView.as_view()),
-    path('strana/<int:strana>', views.PostListView.as_view()),
-    path('tag/<str:tag>', views.PostListView.as_view()),
-    path('tag/<str:tag>/strana/<int:strana>', views.PostListView.as_view()),
-    path('tag/<str:tag>/page/<int:page>', views.PostListView.as_view()),
-    path('search/<str:search>', views.PostListView.as_view()),
-    path('hledej/<str:hledej>', views.PostListView.as_view()),
-    path('search/<str:search>/strana/<int:strana>', views.PostListView.as_view()),
-    path('hledej/<str:hledej>/page/<int:page>', views.PostListView.as_view()),
+    path('page/<int:page>/', views.PostListView.as_view()),
+    path('strana/<int:strana>/', views.PostListView.as_view()),
+    path('tag/<str:tag>/', views.PostListView.as_view()),
+    path('tag/<str:tag>/strana/<int:strana>/', views.PostListView.as_view()),
+    path('tag/<str:tag>/page/<int:page>/', views.PostListView.as_view()),
+    path('search/<str:search>/', views.PostListView.as_view()),
+    path('hledej/<str:hledej>/', views.PostListView.as_view()),
+    path('search/<str:search>/strana/<int:strana>/', views.PostListView.as_view()),
+    path('hledej/<str:hledej>/page/<int:page>/', views.PostListView.as_view()),
     path('<slug:url_cze>/', views.PostDetailView.as_view()),
 ]
