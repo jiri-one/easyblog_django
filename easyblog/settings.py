5628148fcae6ba7a91930cc00cc64c3e1de2b722
jiri.one <nemec@jiri.one>
2022-09-17 22:27:09 +0200

    updated ALLOWED_HOSTS in settings.py

--- easyblog/settings.py
+++ easyblog/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
