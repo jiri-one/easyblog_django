6555a613139e5e37e34ec3be9273e6a593df6446
jiri.one <nemec@jiri.one>
2023-01-09 20:48:46 +0100

    WIP implement args

--- easyblog/settings.py
+++ easyblog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,7 +174,9 @@ EASYCRON_TASKS = [
         'name': 'dump sql',
         'app': 'django.core', 
         'command': 'dumpdata',
-        'args': '', # TODO: implement args
+        'args': ['jiri_one',
+                 '-o',
+                 f'{datetime.now().strftime('%y-%m-%d_%H:%M')}'], # TODO: implement args
         'schedule': timedelta(seconds=3),
     },
 ]
