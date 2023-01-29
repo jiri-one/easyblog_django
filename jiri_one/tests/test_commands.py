e0b7c609dc3595cf02f99fc1cacc710bcf7cc6bf
jiri.one <nemec@jiri.one>
2023-01-29 09:13:25 +0100

    added test_fix_internal_urls

--- jiri_one/tests/test_commands.py
+++ jiri_one/tests/test_commands.py
@@ -7,6 +7,15 @@ import pytest
 from django.conf import settings
 from jiri_one.models import Post, Author
 
+# FIXTURES for tests
+@pytest.fixture
+def fix_me_post() -> Post:
+    """Create a post with with bad html link."""
+    author, _ = Author.objects.get_or_create(nick="Test", defaults={"nick": "Test", "first_name": "Test", "last_name": "Test"})
+    content = """<a href="/soubory/soubor.neco">soubor</a>"""
+    return Post.objects.create(title_cze="bad html", content_cze=content, author=author)
+
+# TESTS
 
 def test_del_old_dbbackup_without_files(tmp_path: Path, monkeypatch):
     monkeypatch.setattr(settings, "DB_BACKUP_DIR", tmp_path)
@@ -24,3 +33,14 @@ def test_del_old_dbbackup_with_fourteen_files(tmp_path: Path, monkeypatch):
     assert len([x for x in tmp_path.iterdir()]) == 14
     call_command('del_old_dbbackup', interactive=False, stdout=out)
     assert len([x for x in tmp_path.iterdir()]) == 10
+
+
+@pytest.mark.django_db
+def test_fix_internal_urls(fix_me_post):
+    out = StringIO()
+    assert isinstance(fix_me_post, Post)
+    assert "/soubory" in fix_me_post.content_cze
+    call_command('fix_internal_urls', stdout=out)
+    fix_me_post.refresh_from_db() # we need refresh this instance from DB
+    assert "/soubory" not in fix_me_post.content_cze
+    assert "/files" in fix_me_post.content_cze
