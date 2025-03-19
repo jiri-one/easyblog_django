5e820f644108aeb6bacf5aabd6f27fc3f507e532
jiri-one <nemec@jiri.one>
2025-03-19 11:43:07 +0100

    updated custom_template_filters.py

--- jiri_one/templatetags/custom_template_filters.py
+++ jiri_one/templatetags/custom_template_filters.py
@@ -4,15 +4,6 @@ register = Library()
 
 
 @register.filter
-def tags_html(tags):
-    """From tags of post create HTML"""
-    tags_links = []
-    for tag in tags.values():
-        tags_links.append(f"""<a href="/tag/{tag["url_cze"]}">{tag["name_cze"]}</a>""")
-    return ", ".join(tags_links)
-
-
-@register.filter
 def rm_pg_nr(path: str):
     """From url path remove the last part, which is page number"""
     return "/".join(list(filter(None, path.split("/")))[:-1])
