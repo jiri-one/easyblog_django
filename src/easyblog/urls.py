c3f7553a97e463148840b48c63c861aad5fb43b0
jiri-one <nemec@jiri.one>
2025-07-27 11:05:24 +0200

    fixed urls.py

--- src/easyblog/urls.py
+++ src/easyblog/urls.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
