9cb2a6226f08a0e2140e84c65d58a58e41fb4f25
jiri-one <nemec@jiri.one>
2025-07-30 10:10:25 +0200

    Add test test_random_post_by_tags_graphql_query

--- src/jiri_one/tests/test_graphql.py
+++ src/jiri_one/tests/test_graphql.py
@@ -41,6 +41,17 @@ def create_random_tags():
     yield tags
 
 
+@pytest.fixture
+def create_random_posts_with_tags(create_random_posts, create_random_tags):
+    tags: list[Tag] = create_random_tags
+    posts:list[Post] = create_random_posts
+    for post in posts:
+        post: Post
+        post_tags = random.choices(tags, k=3)
+        post.tags.add(*post_tags)
+        post.save()
+    return posts, tags
+
 @pytest.mark.django_db
 def test_all_posts_graphql_query(create_random_posts):
     posts = list[dict[str, str]]()
@@ -104,6 +115,39 @@ def test_random_post_by_id_graphql_query(create_random_posts):
         graphql_post = response["data"]["postById"]
         assert graphql_post["titleCze"] == post.title_cze
         assert graphql_post["contentCze"] == post.content_cze
+
+
+@pytest.mark.django_db
+def test_random_post_by_tags_graphql_query(create_random_posts_with_tags):
+    client = Client(schema)
+    posts, tags = create_random_posts_with_tags
+    posts: list[Post]
+    tags: list[Tag]
+    for _ in range(3):
+        tags_to_filter = random.choices(tags, k=2)
+        filtered_posts = Post.objects.filter(tags__in=tags_to_filter)
+        post_data_list = list[dict[str, str]]()
+        for post in filtered_posts:
+            post_data_list.append(
+                dict(
+                    id=post.id,
+                    titleCze=post.title_cze,
+                    contentCze=post.content_cze,
+                )
+            )
+        query = """
+        query {
+            postsByTagsUrl(tagUrls: ["TAG1", "TAG2"]) {
+                titleCze
+                contentCze
+                id
+            }
+        }
+        """.replace("TAG1", tags_to_filter[0].url_cze).replace("TAG2", tags_to_filter[1].url_cze)
+        response = client.execute(query)
+        assert response is not None and "data" in response
+        graphql_posts = sorted(response["data"]["postsByTagsUrl"], key=lambda post: post["id"])
+        assert graphql_posts == sorted(post_data_list, key=lambda post: post["id"])
 
 
 @pytest.mark.django_db
