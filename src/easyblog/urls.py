27f2e432a630c30c893294e9be09eb679d4411d3
jiri-one <nemec@jiri.one>
2025-08-12 16:54:18 +0200

    only reformated and sort imports

--- src/easyblog/urls.py
+++ src/easyblog/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from graphene_django.views import GraphQLView
+from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
